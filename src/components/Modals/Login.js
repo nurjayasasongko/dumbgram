945deb57d20ee5fdc0a84224ee62d8210b261369
nurjayasasongko <afnurjayas@gmail.com>
2022-01-13 08:29:04 +0700

    add Login

--- src/components/Modals/Login.js
+++ src/components/Modals/Login.js
@@ -1,14 +1,13 @@
 import { Dialog, Transition } from '@headlessui/react'
 import { Fragment, useState } from 'react'
 
-function RegisterModal() {
+function LoginModal() {
   let [isOpen, setIsOpen] = useState(false)
 
-  function closeRegModal() {
+  function closeModal() {
     setIsOpen(false)
   }
-
-  function openRegModal() {
+  function openModal() {
     setIsOpen(true)
   }
 
@@ -16,17 +15,17 @@ function RegisterModal() {
     <>
       <button
         type="button"
-        onClick={openRegModal}
-        className="text-[#B7B7B7] text-xs font-[Metropolis] px-6 py-1"
+        onClick={openModal}
+        className="text-white font-[Metropolis] text-xs bg-gradient-to-r from-[#4DD4F7] via-[#9090FB] via-[#FF6B81] to-[#FBDF63] rounded px-6 py-1"
         >
-      Register
+      Login
       </button>
 
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog
           as="div"
           className="fixed inset-0 z-10 overflow-y-auto"
-          onClose={closeRegModal}
+          onClose={closeModal}
         >
           <div className="min-h-screen px-4 text-center bg-[#D2D2D2]/30">
             <Transition.Child
@@ -61,29 +60,23 @@ function RegisterModal() {
                   <button //Close Button
                     type="button"
                     className="absolute top-2 right-2 px-4 py-2 text-sm font-medium text-[#B1B1B1]"
-                    onClick={closeRegModal}
+                    onClick={closeModal}
                   > X </button>
                   {/* Tittle of Modal */}
                   <Dialog.Title as="h3"className="text-3xl font-[Metropolis] font-bold text-white p-6 text-center">
-                  Register</Dialog.Title>
+                  Login</Dialog.Title>
                 {/* Form */}
                 <form class="px-6 pb-4 space-y-6 lg:px-8 sm:pb-6 xl:pb-8" action=" ">
                   <div>
                     <input type="email" placeholder="Email" class="bg-[#D2D2D2]/25 border border-[#D2D2D2] text-[#B1B1B1] text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required/>
                   </div>
                   <div>
-                    <input type="text" placeholder="Name" class="bg-[#D2D2D2]/25 border border-[#D2D2D2] text-[#B1B1B1] text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " required/>
-                  </div>
-                  <div>
-                    <input type="text" placeholder="Username" class="bg-[#D2D2D2]/25 border border-[#D2D2D2] text-[#B1B1B1] text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required/>
-                  </div>
-                  <div>
                     <input type="password"  placeholder="Password" class="bg-[#D2D2D2]/25 border border-[#D2D2D2] text-[#B1B1B1] text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required/>
                   </div>
-                  {/* Register Button */}
+                  {/* Login Button */}
                   <button type="submit" class="w-full p-2.5 mb-0 text-white font-[Metropolis] font-extrabold bg-gradient-to-r from-[#4DD4F7] via-[#9090FB] via-[#FF6B81] to-[#FBDF63] rounded text-center">Register</button>
                   <div class="text-sm font-extralight font-[Metropolis] text-[#B1B1B1] text-center">
-                    Already have an account ? Click <a href=" " class="text-[#B1B1B1] font-bold hover:underline">Here</a>
+                    Don't have an account ? Click <a class="text-[#B1B1B1] font-bold hover:underline">Here</a>
                   </div>
                 </form>
               </div>
@@ -95,4 +88,4 @@ function RegisterModal() {
   )
 }
 
-export default RegisterModal;
+export default LoginModal
